66e39fc061afa692a8e49616ca74921167b5e0a8
Henrik Nyh <henrik@nyh.se>
2013-09-11 21:18:52 +0200

    Cleanup.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -1,13 +1,13 @@
 require "i18n_utils"
 
 describe I18nUtils do
-  let(:view) { double.extend(I18nUtils::All) }
+  include I18nUtils::All
 
   describe "t_attribute" do
     it "uses human_attribute_name" do
       klass = double
       allow(klass).to receive(:human_attribute_name).with(:foo).and_return("foux du fafa")
-      view.t_attribute(klass, :foo).should == "foux du fafa"
+      t_attribute(klass, :foo).should == "foux du fafa"
       I18nUtils.t_attribute(klass, :foo).should == "foux du fafa"
     end
   end
@@ -16,7 +16,7 @@ describe I18nUtils do
     it "uses model_name.human" do
       klass = double
       klass.stub_chain(:model_name, :human).and_return("Clase")
-      view.t_model(klass).should == "Clase"
+      t_model(klass).should == "Clase"
       I18nUtils.t_model(klass).should == "Clase"
     end
   end
@@ -29,17 +29,17 @@ describe I18nUtils do
     end
 
     it "captures content and abbreviates scopes" do
-      expect(view).to receive(:capture) { |&block| block.call }
+      expect(self).to receive(:capture) { |&block| block.call }
 
-      expect(view).to receive(:t).
+      expect(self).to receive(:t).
         with("sign_in_now.text", sign_in: %{<a href="url">Sign in</a>}).
         and_return("output")
 
-      expect(view).to receive(:t).
+      expect(self).to receive(:t).
         with("sign_in_now.sign_in", {}).
         and_return("Sign in")
 
-      view.t_scope("sign_in_now.text") do |scope|
+      t_scope("sign_in_now.text") do |scope|
         scope.sign_in do
           link_to(scope.t(:sign_in), "url")
         end
